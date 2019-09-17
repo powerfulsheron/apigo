bf400b44571eac6748e76cc270197f03b9c04fb4
lorenzo <lcanavaggio.ext@wedigital.garden>
2019-09-18 01:49:57 +0200

    feature(JWT): WIP code refactoring and jwt implementation

---
+++ back/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"apigo/middleware"
+	"os"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
+	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
+	router.Use(app.JwtAuthentication) //attach JWT auth middleware
+
+	port := os.Getenv("PORT") //Get port from .env file, we did not specify any port so this should return an empty string when tested locally
+	if port == "" {
+		port = "8000" //localhost
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":" + port, router) //Launch the app, visit localhost:8000/api
+	if err != nil {
+		fmt.Print(err)
+	}
+}
