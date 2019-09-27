37842687c4ae3a55476b596e80ec0e1b337bb448
Maxime AUBLET <maximeaublet@gmail.com>
2019-09-27 15:28:31 +0200

    feature(Login): bug fix

--- back/main.go
+++ back/main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"apigo/back/controllers"
 	"apigo/back/middleware"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
 	router := gin.Default()
-	router.POST("/login", func(c *gin.Context){
+
+	// Login route
+	router.POST("/new", func(c *gin.Context) {
 		controllers.CreateUser(c.Writer, c.Request)
 	})
-	router.Use(middleware.JwtAuthentication) //attach JWT auth middleware
+
+	router.POST("/login", func(c *gin.Context) {
+		controllers.Authenticate(c.Writer, c.Request)
+	})
+
+	// Vote route
+	router.Use(middleware.JwtAuthentication) // attach JWT auth middleware
 	router.Run(os.Getenv("api_port"))
 }
