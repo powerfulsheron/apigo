a56594d8fd1fee50114df827989b567573d69cf9
Lorenzo Canavaggio <lcanavaggio.ext@wedigital.garden>
2019-09-27 20:51:14 +0200

    feature(uuid):Uuid feature done

--- back/models/uuid.go
+++ back/models/uuid.go
@@ -3,17 +3,18 @@ package models
 import (
     "github.com/jinzhu/gorm"
     uuid "github.com/satori/go.uuid"
-    "time"
+	"fmt"
 )
 
 type Uuid struct {
-    ID *uuid.UUID `gorm:"primary_key;unique;type:uuid;column:id"json:"id"`
-    CreatedAt time.Time `json:"created_at"`
-    UpdatedAt time.Time `json:"updated_at"`
-    DeletedAt *time.Time `sql:"index"json:"deleted_at"`
+    UUID *uuid.UUID `gorm:"unique;type:uuid;column:uuid"json:"uuid"`
 }
 
-func (b *Base) BeforeCreate(scope *gorm.Scope) error {
-    scope.SetColumn("ID", uuid.NewV4().String())
-    return nil
+func (b *Uuid) BeforeCreate(scope *gorm.Scope) error {
+	u, err := uuid.NewV4()
+	scope.SetColumn("UUID", u.String())
+	if err != nil {
+		fmt.Println(err)
+	}
+	return nil
 }
--- back/models/votes.go
+++ back/models/votes.go
@@ -7,8 +7,7 @@ import (
 // Vote a struct relative to a votes
 type Vote struct {
 	gorm.Model
-	ID          uint   `gorm:"primary_key"json:"-"`
-	UUID        string `gorm:"unique_index"json:"uuid"`
+	Uuid
 	Title       string `gorm:"size:255"json:"Title"`
 	Description string `gorm:"size:1023"json:"Description"`
 }
