f5a52d41eae43158bdda25e8e557f755416eda2f
powerfulsheron <lorenzo.canavaggio@laposte.net>
2019-10-21 22:09:08 +0200

    feature(test):Login user test and makefile rework for faster test launch

--- back/tests/users_test.go
+++ back/tests/users_test.go
@@ -57,6 +57,57 @@ func TestCreateUser(t *testing.T) {
 
 	if got != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+			got, expected)
+	}
+}
+
+func TestUserLogin(t *testing.T) { 
+
+	userMock := UserMock{}
+	userMock.AccessLevel = 0
+	err := faker.FakeData(&userMock)
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	var jsonStr = []byte(`{"email":"`+userMock.Email+`","pass":"`+userMock.Password+`"}`)
+
+	createReq, err := http.NewRequest("POST", "/users", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	createReq.Header.Set("Content-Type", "application/json")
+	createRecorder := httptest.NewRecorder()
+	createHandler := http.HandlerFunc(controllers.CreateUser)
+	createHandler.ServeHTTP(createRecorder, createReq)
+	if status := createRecorder.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	loginReq, err := http.NewRequest("POST", "/login", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	loginReq.Header.Set("Content-Type", "application/json")
+	loginRecorder := httptest.NewRecorder()
+	loginHandler := http.HandlerFunc(controllers.Authenticate)
+	loginHandler.ServeHTTP(loginRecorder, loginReq)
+	if status := loginRecorder.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	var responseBodyArray map[string]interface{}
+	if err := json.Unmarshal(loginRecorder.Body.Bytes(), &responseBodyArray); err != nil {
+		t.Fatal(err)
+	}
+
+	got := `{"message":"`+responseBodyArray["message"].(string)+`","status":`+ fmt.Sprintf("%t", responseBodyArray["status"].(bool))+`}`
+	expected := `{"message":"Logged In","status":true}`
+
+	if got != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			got, expected)
 	}
 }
